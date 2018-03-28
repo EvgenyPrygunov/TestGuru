c2694a02d2c45a0f2b8fa17396cfa0ecdbb0ea4c
Evgeny Prygunov <prygunov.ea@gmail.com>
2018-03-29 00:25:18 +0300

    Lesson 3. Added references to questions and tests.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328194940) do
+ActiveRecord::Schema.define(version: 20180328212235) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -27,17 +27,19 @@ ActiveRecord::Schema.define(version: 20180328194940) do
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "test_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "test_id", null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
-    t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
