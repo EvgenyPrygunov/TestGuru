60474d0d645afbf114615a9a6e40f3b3c47078a1
Evgeny Prygunov <prygunov.ea@gmail.com>
2018-04-14 22:10:18 +0300

    Lesson 6

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :test
   has_many :answers
+
+  validates :body, presence: true
+  validates :answers, numericality: { greater_than: 0, less_than: 5 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409204400) do
+ActiveRecord::Schema.define(version: 20180414183506) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180409204400) do
     t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
